ba4fb6f52eb25ce8eb2f242df9c18eadbabf49f0
Samuel Caçador <samuelmoreira27@gmail.com>
2022-02-19 13:41:52 +0000

    feat: convert graphql skeleton to rest skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
 import router from './router';
-import { apolloServer } from './services/ApolloServices/ApolloServerService';
 
 const app = express();
 
-apolloServer
-    .start()
-    .then(() => {
-        app.use(express.json());
-        app.use(apolloServer.getMiddleware());
-        app.use(router);
+app.use(express.json());
+app.use(router);
 
-        app.listen(process.env.PORT || 3333, () => {
-            console.log(`🚀 App started on port ${process.env.PORT || 3333} - Hello ${process.env.HELLO}`);
-        });
-    })
-    .catch(err => {
-        console.log(err);
-    });
+app.listen(process.env.PORT || 3333, () => {
+    console.log(`🚀 App started on port ${process.env.PORT || 3333} - Hello ${process.env.HELLO}`);
+});
